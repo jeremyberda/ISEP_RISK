60a189412717d45741de81960b872cb23fe2d63c
JeremyBerda <jeremy.berda@icloud.com>
2018-05-26 17:57:08 +0200

    Ca avance trèèès lourd

--- src/Region.java
+++ src/Region.java
@@ -6,8 +6,24 @@ public class Region {
 
     private ArrayList<Territory> territoriesOfRegion = new ArrayList<Territory>();
 
+    //constructor
+
     public Region(int regionID, ArrayList<Territory> territoriesOfRegion) {
         this.regionID = regionID;
         this.territoriesOfRegion = territoriesOfRegion;
+    }
+
+    //on fait une fonction qui vérifie si un joueur contrôle une région entière, ou pas
+
+    public boolean isControlledBy(Player player) {
+
+        int bonus = 0;
+
+        for (int i = 0; i<this.territoriesOfRegion.size();i++) {
+
+            if (this.territoriesOfRegion.get(i).)
+        }
+
+
     }
 }
--- src/Territory.java
+++ src/Territory.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+
+
 public class Territory {
+
+    private int territoryID;
+
+    private int[] adjacentTerritoriesID;
+
+
+
+    public ArrayList<Unity> unitiesList = new ArrayList<Unity>();
+
+    ArrayList<Unity> fightUnitiesList = new ArrayList<Unity>();
+
+    private Player owner;
+
+
+    public Territory(int territoryID, int[] adjacentTerritoriesID) {
+        this.territoryID = territoryID;
+        this.adjacentTerritoriesID = adjacentTerritoriesID;
+    }
+
 }
